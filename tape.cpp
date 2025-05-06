2edbef84be9c8f39f7e243d2a6fedc33af6cf31e
Local Piper <artsor53@gmail.com>
2025-05-06 21:29:18 +0300

    chore: tape class

---
+++ tape.cpp
@@ -0,0 +1,66 @@
+
+#include "tape.hpp"
+#include <string>
+Tape::Tape(const std::string &filename) : filename(filename) { load(); }
+
+Tape::~Tape() { save(); }
+
+void Tape::load() {
+  file.open(filename, std::ios::in);
+  buffer.clear();
+  int val;
+
+  while (file >> val) {
+    buffer.push_back(val);
+  }
+
+  file.close();
+  head = 0;
+}
+
+void Tape::save() {
+  file.open(filename, std::ios::out | std::ios::trunc);
+  for (int val : buffer) {
+    file << val << "\n";
+  }
+  file.close();
+}
+
+bool Tape::read(int &value) {
+  if (head < buffer.size()) {
+    value = buffer[head];
+    return true;
+  }
+  return false;
+}
+
+bool Tape::write(int value) {
+  if (head < buffer.size()) {
+    buffer[head] = value;
+  } else {
+    buffer.push_back(value);
+  }
+  return true;
+}
+
+bool Tape::move_forward() {
+  if (head + 1 <= buffer.size()) {
+    ++head;
+    return true;
+  }
+  return false;
+}
+
+bool Tape::move_backward() {
+  if (head == 0)
+    return false;
+  --head;
+  return true;
+}
+
+bool Tape::rewind() {
+  head = 0;
+  return true;
+}
+
+void Tape::close() { save(); }
